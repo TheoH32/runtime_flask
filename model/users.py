9aa12096c8d2b00225abc94ab62a9300281b6023
Theo Huntalas <theo.h131@gmail.com>
2023-04-12 18:16:24 -0700

    fixing imports

--- model/users.py
+++ model/users.py
@@ -1,31 +1,42 @@
-from random import randrange
-from datetime import date
-import os, base64
+"""
+These imports define the key object
+"""
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import json
 
-from __init__ import app, db
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 
-# Define the User class to manage actions in the 'users' table
-# -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
-# -- a.) db.Model is like an inner layer of the onion in ORM
-# -- b.) User represents data we want to store, something that is built on db.Model
-# -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL
+# Setup of key Flask object (app)
+app = Flask(__name__)
+# Setup SQLAlchemy object and properties for the database (db)
+database = 'sqlite:///sqlite.db'  # path and filename of database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = database
+app.config['SECRET_KEY'] = 'SECRET_KEY'
+db = SQLAlchemy()
+db.init_app(app)
+
 class User(db.Model):
     __tablename__ = 'users'  # table name is plural, class name is singular
 
     # Define the User schema with "vars" from object
+    # Attributes used for future defined users
     id = db.Column(db.Integer, primary_key=True)
     _name = db.Column(db.String(255), unique=False, nullable=False)
     _uid = db.Column(db.String(255), unique=True, nullable=False)
     _password = db.Column(db.String(255), unique=False, nullable=False)
 
     # constructor of a User object, initializes the instance variables within object (self)
+    # The constructed used to instantiate an object from our user class
     def __init__(self, name, uid, password="123qwerty"):
         self._name = name    # variables with self prefix become part of the object, 
         self._uid = uid
         self.set_password(password)
+        
+
 
     # a name getter method, extracts name from object
     @property
@@ -37,12 +48,12 @@ class User(db.Model):
     def name(self, name):
         self._name = name
     
-    # a getter method, extracts email from object
+    # a getter method, extracts uid from object
     @property
     def uid(self):
         return self._uid
     
-    # a setter function, allows name to be updated after initial object creation
+    # a setter function, allows uid to be updated after initial object creation
     @uid.setter
     def uid(self, uid):
         self._uid = uid
@@ -55,18 +66,19 @@ class User(db.Model):
     def password(self):
         return self._password[0:10] + "..." # because of security only show 1st characters
 
-    # update password, this is conventional setter
+    # update password, this is conventional method used for setter
     def set_password(self, password):
         """Create a hashed password."""
         self._password = generate_password_hash(password, method='sha256')
 
-    # check password parameter versus stored/encrypted password
+    # check password parameter against stored/encrypted password
     def is_password(self, password):
         """Check against hashed password."""
         result = check_password_hash(self._password, password)
         return result
+       
     
-    # output content using str(object) in human readable form, uses getter
+    # output content using str(object) is in human readable form
     # output content using json dumps, this is ready for API response
     def __str__(self):
         return json.dumps(self.read())
@@ -102,6 +114,7 @@ class User(db.Model):
             self.uid = uid
         if len(password) > 0:
             self.set_password(password)
+        db.session.add(self) # performs update when id exists\n",
         db.session.commit()
         return self
 
@@ -111,12 +124,12 @@ class User(db.Model):
         db.session.delete(self)
         db.session.commit()
         return None
-
+       
 
 """Database Creation and Testing """
 
 
-# Builds working data for testing
+# Builds working user data
 def initUsers():
     with app.app_context():
         """Create database and tables"""
@@ -131,15 +144,15 @@ def initUsers():
 
         users = [u1, u2, u3, u4, u5, u6]
 
+
         """Builds sample user/note(s) data"""
         for user in users:
             try:
-                '''add user/post data to table'''
-                user.create()
-            except IntegrityError:
+                '''add user to table'''
+                object = user.create()
+                print(f"Created new uid {object.uid}")
+            except:  # error raised if object nit created
                 '''fails with bad or duplicate data'''
-                db.session.remove()
-                print(f"Records exist, duplicate email, or error: {user.uid}")
-
+                print(f"Records exist uid {user.uid}, or error.")
+                
 initUsers()
-            
